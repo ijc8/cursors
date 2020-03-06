7ff89c2a2de49cf0e9aba4dddd52932a796266c7
Ian Clester <ijc@mit.edu>
2020-03-06 11:03:48 -0500

    Refactor to separate state from UI.

--- cursors.py
+++ cursors.py
@@ -6,17 +6,11 @@ import matplotlib.pyplot as plt
 import time
 import copy
 
-shape = (8, 16)
-# MxNx2; first MxN is sequencer layer, second is 'active'/'playing field' layer.
-# This describes state, *not* appearence.
-grid = np.zeros(shape + (2,), dtype=np.int)
 # Each cursor consists of a row, a height, a speed, and a position (column). ... and merge direction
 # The first three things are fixed-ish, the last changes all the time.
 # In particular, the last is a float which is rounded down for rendering and checking.
-# TODO make it a claaaaass
-# Idea: color spectrum across y axis; actual cursor color is average of colors in its rows.
-# equivalently take average of start, start + height, compute color. okay yeah let's do that.
-# another idea: separate merge and destroy effectors. destroy good for reducing complexity in a more serious way.
+
+# idea: separate merge and destroy effectors. destroy good for reducing complexity in a more serious way.
 # alternatively, mute/unmute effector, allowing for same thing in a less permanent way.
 class Cursor:
     def __init__(self, start, height, speed, pos, merge_direction):
@@ -30,7 +24,44 @@ class Cursor:
         return f"Cursor(start={self.start}, height={self.height}, speed={self.speed}, pos={self.pos}, merge_direction={self.merge_direction})"
 
 proto_cursor = Cursor(0, 8, 3, 0, 1)
-cursors = [copy.copy(proto_cursor)]
+
+class GameState:
+    def __init__(self, n=2):
+        """Setup the game state.
+
+        n is the number of consecutive square grids (probably equal to the number of players).
+        """
+        self.cursors = [copy.copy(proto_cursor)]
+        shape = (8, 16)
+        # MxNx2; first MxN is sequencer layer, second is 'active'/'playing field' layer.
+        # This describes state, *not* appearence.
+        self.grid = np.zeros(shape + (2,), dtype=np.int)
+
+    def reset_cursors(self):
+        self.cursors = [copy.copy(proto_cursor)]
+
+    def update(self, dt):
+        visited = set()
+        for cursor in self.cursors[:]:
+            old_pos = int(cursor.pos)
+            # NOTE: This does not handle the case where dt is so large that multiple steps have passed.
+            # If dt is too large, cursors will 'teleport' to their new position, skipping any effectors in between.
+            cursor.pos += cursor.speed * dt
+            cursor.pos %= self.grid.shape[1]
+            new_pos = int(cursor.pos)
+            if new_pos != old_pos:
+                hits = self.grid[cursor.start : cursor.start + cursor.height, new_pos, 0].nonzero()[0]
+                for hit in hits:
+                    pos = (cursor.start + hit, new_pos)
+                    if pos in visited:
+                        print(
+                            f"aha, we already hit this effector ({pos}) this round; skipping for avoid merge issue."
+                        )
+                        continue
+                    visited.add(pos)
+                    effector = effectors[self.grid[pos[0], pos[1], 0] - 1]
+                    print(f"we hit {effector.name} at {pos}!")
+                    effector.function(self, cursor, pos)
 
 
 class Effector:
@@ -40,7 +71,7 @@ class Effector:
         self.color = color
 
 
-def reverse(cursor, _):
+def reverse(state, cursor, _):
     if cursor.speed > 0:
         gap = cursor.pos - np.floor(cursor.pos)
         cursor.pos = np.floor(cursor.pos) + (1 - gap)
@@ -50,39 +81,39 @@ def reverse(cursor, _):
     cursor.speed *= -1
 
 
-def split(cursor, pos):
+def split(state, cursor, pos):
     if pos[0] == cursor.start:
         return  # can't split at the top. (think about it)
-    global cursors
-    ind = cursors.index(cursor)
+    # TODO: perhaps pass in cursor index instead of the cursor itself?
+    ind = state.cursors.index(cursor)
     top = Cursor(cursor.start, pos[0] - cursor.start, cursor.speed, cursor.pos, 1)
     bottom = Cursor(pos[0], cursor.start + cursor.height - pos[0], cursor.speed, cursor.pos, -1)
-    cursors[ind] = top
-    cursors.insert(ind + 1, bottom)
+    state.cursors[ind] = top
+    state.cursors.insert(ind + 1, bottom)
 
 
-def merge(cursor, _):
-    ind = cursors.index(cursor)
-    print("old", cursors)
+def merge(state, cursor, _):
+    ind = state.cursors.index(cursor)
+    print("old", state.cursors)
     print("merge", ind, cursor.merge_direction)
     if cursor.merge_direction == -1:
         if ind > 0:
             print("with", ind - 1)
-            cursors[ind - 1].height += cursor.height
+            state.cursors[ind - 1].height += cursor.height
         print("delete")
-        del cursors[ind]
+        del state.cursors[ind]
     elif cursor.merge_direction == 1:
         # Tricky issue: newly extended cursor will be processed after this one, and may register a hit on the same merge node!
         # How to avoid double-merging? One easy answer is that merge nodes disappear after use, but this is unsatisfactory.
         # Given how our system works, only one cursor should be on a row at a time - for it should never be the case that two cursors
         # both hit the same effector in one round. In which case, maybe the easiest thing to do is avoid activating any node twice.
-        if ind < len(cursors) - 1:
+        if ind < len(state.cursors) - 1:
             print("with", ind + 1)
-            cursors[ind + 1].start = cursor.start
-            cursors[ind + 1].height += cursor.height
+            state.cursors[ind + 1].start = cursor.start
+            state.cursors[ind + 1].height += cursor.height
         print("delete")
-        del cursors[ind]
-    print("new", cursors)
+        del state.cursors[ind]
+    print("new", state.cursors)
 
 
 effectors = [
@@ -93,108 +124,91 @@ effectors = [
 selected_effector = effectors[0]
 
 
-def render(grid, cursors):
-    # Render the state grid to an RGB image.
-    g = np.ones(grid.shape[:2] + (3,), dtype=np.float) * [0.6, 0.6, 0.6]
-    for cursor in cursors:
+def render(state):
+    # Render the state to an RGB image.
+    g = np.ones(state.grid.shape[:2] + (3,), dtype=np.float) * [0.6, 0.6, 0.6]
+    for cursor in state.cursors:
         middle = (cursor.start + (cursor.start + cursor.height)) / 2
-        level = (middle - 0.5) / (grid.shape[0] - 1)
+        level = (middle - 0.5) / (state.grid.shape[0] - 1)
         g[cursor.start : cursor.start + cursor.height, int(cursor.pos)] = [level, 0, 1 - level]
-    for r, c, layer in zip(*grid.nonzero()):
-        value = grid[r, c, layer]
+    for r, c, layer in zip(*state.grid.nonzero()):
+        value = state.grid[r, c, layer]
         g[r, c] = effectors[value - 1].color
     return g
 
 
-matplotlib.rcParams["toolbar"] = "None"
-plt.ion()
-fig, ax = plt.subplots()
+def plt_main():
+    state = GameState(2)
+
+    matplotlib.rcParams["toolbar"] = "None"
+    plt.ion()
+    fig, ax = plt.subplots()
 
 
-def on_click(event):
-    if event.xdata is None or event.ydata is None:
-        return
-    x = int(round(event.xdata))
-    y = int(round(event.ydata))
-    print(x, y)
-    grid[y, x] = effectors.index(selected_effector) + 1
-    # g = render(grid, cursors)
-    # im.set_data(g)
+    def on_click(event):
+        if event.xdata is None or event.ydata is None:
+            return
+        x = int(round(event.xdata))
+        y = int(round(event.ydata))
+        print(x, y)
+        state.grid[y, x] = effectors.index(selected_effector) + 1
+        # g = render(grid, cursors)
+        # im.set_data(g)
 
 
-keymap = {
-    "r": effectors[0],
-    "s": effectors[1],
-    # 'w': 'warp',
-    "m": effectors[2],  # or 'join'?
-}
+    keymap = {
+        "r": effectors[0],
+        "s": effectors[1],
+        # 'w': 'warp',
+        "m": effectors[2],  # or 'join'?
+    }
+
+    def on_keypress(event):
+        if event.key == "R":
+            # Reset
+            state.reset_cursors()
+            return
+        if event.key not in keymap:
+            return
+        global selected_effector
+        selected_effector = keymap[event.key]
+        ax.set_xlabel(selected_effector.name)
 
 
-def on_keypress(event):
-    if event.key == "R":
-        # Reset
-        global cursors
-        cursors = [copy.copy(proto_cursor)]
-        return
-    if event.key not in keymap:
-        return
-    global selected_effector
-    selected_effector = keymap[event.key]
-    ax.set_xlabel(selected_effector.name)
+    cid = fig.canvas.mpl_connect("button_press_event", on_click)
+    cid = fig.canvas.mpl_connect("key_press_event", on_keypress)
+
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        top=False,
+        labelbottom=False,
+        right=False,
+        left=False,
+        labelleft=False,
+    )
+    ax.set_xticks(np.arange(state.grid.shape[1] + 1) - 0.5, minor=True)
+    ax.set_yticks(np.arange(state.grid.shape[0] + 1) - 0.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+    im = ax.imshow(state.grid[:, :, 0])
+
+    last = time.time()
+
+    while True:
+        g = render(state)
+        im.set_data(g)
+
+        now = time.time()
+        dt = now - last
+        state.update(dt)
+        last = now
+
+        plt.pause(0.001)
 
 
-cid = fig.canvas.mpl_connect("button_press_event", on_click)
-cid = fig.canvas.mpl_connect("key_press_event", on_keypress)
-
-ax.tick_params(
-    axis="both",
-    which="both",
-    bottom=False,
-    top=False,
-    labelbottom=False,
-    right=False,
-    left=False,
-    labelleft=False,
-)
-ax.set_xticks(np.arange(grid.shape[1] + 1) - 0.5, minor=True)
-ax.set_yticks(np.arange(grid.shape[0] + 1) - 0.5, minor=True)
-ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
-ax.tick_params(which="minor", bottom=False, left=False)
-for edge, spine in ax.spines.items():
-    spine.set_visible(False)
-im = ax.imshow(grid[:, :, 0])
-
-last = time.time()
-
-
-def update():
-    global last
-    now = time.time()
-    dt = now - last
-    visited = set()
-    for cursor in cursors[:]:
-        old_pos = int(cursor.pos)
-        cursor.pos += cursor.speed * dt
-        cursor.pos %= grid.shape[1]
-        new_pos = int(cursor.pos)
-        if new_pos != old_pos:
-            hits = grid[cursor.start : cursor.start + cursor.height, new_pos, 0].nonzero()[0]
-            for hit in hits:
-                pos = (cursor.start + hit, new_pos)
-                if pos in visited:
-                    print(
-                        f"aha, we already hit this effector ({pos}) this round; skipping for avoid merge issue."
-                    )
-                    continue
-                visited.add(pos)
-                effector = effectors[grid[pos[0], pos[1], 0] - 1]
-                print(f"we hit {effector.name} at {pos}!")
-                effector.function(cursor, pos)
-    last = now
-
-
-while True:
-    g = render(grid, cursors)
-    im.set_data(g)
-    update()
-    plt.pause(0.001)
+if __name__ == '__main__':
+    plt_main()
